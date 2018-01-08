11c326749f445573e4577b2f3d6e434c5d63cfb7
Valentino Valentin <valentinovalentine95@gmail.com>
2018-01-08 13:46:40 -0700

    my half of testing files

--- test/SevenHour.test.js
+++ test/SevenHour.test.js
@@ -3,13 +3,11 @@ import { mount, shallow } from 'enzyme';
 import SevenHour from '../lib/SevenHour.js';
 import MockData from '../__mocks__/mockData';
 
-describe('App', () => {
+describe('SevenHour', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<SevenHour hour={MockData.hourly_forecast[0].FCTTIME.civil}
-                                 image={MockData.hourly_forecast[0].icon_url}
-                                 temp={MockData.hourly_forecast[0].temp.english}
+    wrapper = shallow(<SevenHour hourForecastArray = {MockData.hourly_forecast}
                       />)
   })
 
@@ -18,17 +16,7 @@ describe('App', () => {
   })
 
   it('should render a card', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('Card').length).toEqual(7);
   })
 
-  it('should render the time, the image, and the temp', () => {
-    expect(wrapper.find('h2').length).toEqual(1);
-    expect(wrapper.find('img').length).toEqual(1);
-    expect(wrapper.find('h3').length).toEqual(1);
-
-    expect(wrapper.find('h2').text()).toEqual(' Time: 12:00 PM ');
-    expect(wrapper.find('img').prop('src')).toEqual('http://icons.wxug.com/i/c/k/partlycloudy.gif');
-    expect(wrapper.find('h3').text()).toEqual(' Projected Temp: 47 ');
-
-  })
 })
