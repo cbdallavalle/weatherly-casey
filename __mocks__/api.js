7cd6affd65593252efbced5606f376436bc57299
Casey Dallavalle <cbdallavalle@gmail.com>
2018-01-10 15:05:17 -0700

    Add HTML checks, add sad path test, add api key into own file and put into git ignore

--- __mocks__/api.js
+++ __mocks__/api.js
@@ -1,6 +1,8 @@
+import apikey from '../__mocks__/apikey';
+
 export default {
   returnWeatherAPI(zip) {
-    return fetch(`http://api.wunderground.com/api/143dda37475fd161/conditions/forecast/forecast10day/hourly/q/${zip}.json`)
-      .then(response => response.json()) 
-      }
+    return fetch(`http://api.wunderground.com/api/${apikey}/conditions/forecast/forecast10day/hourly/q/${zip}.json`)
+      .then(response => response.json())
+  }
 }
--- __mocks__/mockData.js
+++ __mocks__/mockData.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const data = {
   response: {
     version: '0.1',
